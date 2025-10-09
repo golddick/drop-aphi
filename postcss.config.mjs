7a9c3338d2bef5974baf59f53d8b4b1676337213
gold <golddick60@gmail.com>
2025-10-09 17:32:24 +0100

    fish

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {}, // <--- Use this instead of 'tailwindcss'
-    autoprefixer: {},           // recommended
-  },
-};
-
-export default config;
